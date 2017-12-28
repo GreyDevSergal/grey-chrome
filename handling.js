db92b6476308d426c95037bf3981fe32b7975a47
Will Hodge <will.hodge18@gmail.com>
2017-12-27 22:01:15 -0800

    primed eventlistener

--- handling.js
+++ handling.js
@@ -5,46 +5,58 @@
 var activated = false;
 
 function activate() {
-  activated = true;
-  console.log('Greyscale activated');
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     tabs.forEach(function(tab) {
       chrome.tabs.executeScript(tab.id, {
         file: "apply.js",
         allFrames: true
+      }, result => {
+        const lastErr = chrome.runtime.lastError;
+        if (lastErr) console.log('Cannot change tabs with URLs of chrome:// to greyscale.');
       });
     });
   });
 
-  addGreyListener();
+  chrome.tabs.onUpdated.addListener(pageLoaded);
+  activated = true;
+  chrome.browserAction.setIcon({
+    path : "icons/grey48.png"
+  });
+  console.log('Greyscale activated');
 }
 
-function addGreyListener() {
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(changeInfo.status == 'complete'){
-      chrome.tabs.executeScript(tabId, {
-        file: "apply.js",
-        allFrames: true
-      });
-    }
-  });
+function pageLoaded(tabId, changeInfo, tab) {
+  if(changeInfo.status == 'complete'){
+    chrome.tabs.executeScript(tabId, {
+      file: "apply.js",
+      allFrames: true
+    }, result => {
+      const lastErr = chrome.runtime.lastError;
+      if (lastErr) console.log('Cannot change tabs with URLs of chrome:// to greyscale.');
+    });
+  }
 }
 
 function deactivate() {
-  activated = false;
-  console.log('Greyscale deactivated');
   chrome.tabs.query({currentWindow: true}, function(tabs) {
     tabs.forEach(function(tab) {
       chrome.tabs.executeScript(tab.id, {
         file: "revert.js",
         allFrames: true
+      }, result => {
+        const lastErr = chrome.runtime.lastError;
+        if (lastErr) console.log('Cannot change tabs with URLs of chrome:// to greyscale.');
       });
     });
   });
 
-  chrome.tabs.onUpdated.removeListener(addGreyListener);
+  chrome.tabs.onUpdated.removeListener(pageLoaded);
+  activated = false;
+  chrome.browserAction.setIcon({
+    path : "icons/icon48.png"
+  });
+  console.log('Greyscale deactivated');
 }
-
 
 chrome.browserAction.onClicked.addListener(function() {
   if(!activated){
