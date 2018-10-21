e03b4bdf69e08f52461f5b40362c45680e195034
Grey Dev <luukwauben@hotmail.nl>
2018-10-22 00:31:13 +0200

    Update apply.js

--- apply.js
+++ apply.js
@@ -2,7 +2,7 @@
 // Will Hodge
 // Edits the filter style to greyscale for all browsers
 
-var body = document.body;
+let body = document.body;
 body.style['filter'] = 'progid:DXImageTransform.Microsoft.BasicImage(grayScale=1)'; // IE
 body.style['filter'] = 'grayscale(1)'; // Firefox
 body.style['-webkit-filter'] = 'grayscale(1)'; // Safari + Chrome
